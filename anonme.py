4e182bda8d4687355f4bc0225f68af274a816dd8
x0Dsj <85474922+x0Dsj@users.noreply.github.com>
2021-06-30 16:48:59 +0200

    Add files via upload

--- anonme.py
+++ anonme.py
@@ -10,7 +10,6 @@ def MainMenu():
     print(colored(Main_Menu_Banner, 'magenta'))
     print(colored('[+] Checking IP ...\n', 'green'))
     print(colored(requests.get('http://httpbin.org/ip').text, 'green'))
-    
     print(colored('1. Start Tor', 'yellow'))
     print(colored('2. Stop Tor', 'yellow'))
     print(colored('3. Chose your Tor Configuration', 'yellow'))
@@ -20,10 +19,13 @@ def MainMenu():
     
     while True:
         try:
+            
             selection = int(input("Enter Choice : "))
+            
             if selection == 1:
                 Start_Tor()
                 break
+            
             elif selection == 2:
                 Stop_Tor()
                 break
@@ -33,73 +35,36 @@ def MainMenu():
             elif selection == 4:
                 Check_IP()
                 break
-        
+            
             elif selection == 5:
                 Quit()
                 break
+            
             else:
                 print(colored('\nInvalide choise. Enter 1-5', 'red'))
                 MainMenu()
+        
         except ValueError:
             print(colored('\nInvalide choise. Enter 1-5', 'red'))
     exit
 
 
 def Start_Tor():
-    bash_start_tor = """\
-    # destinations you don't want routed through Tor
-    NON_TOR="192.168.1.0/24 192.168.0.0/24"
-
-    # the UID Tor runs as
-    readonly TOR_UID="$(id -u debian-tor)"
-
-    # Tor's TransPort
-    TRANS_PORT="9040"
-
-    sudo service tor stop
-
-    sudo iptables -F
-    sudo iptables -t nat -F
-
-    sudo iptables -t nat -A OUTPUT -m owner --uid-owner $TOR_UID -j RETURN
-    sudo iptables -t nat -A OUTPUT -p udp --dport 53 -j REDIRECT --to-ports 53
-    for NET in $NON_TOR 127.0.0.0/9 127.128.0.0/10; do
-        sudo iptables -t nat -A OUTPUT -d $NET -j RETURN
-    done
-    sudo iptables -t nat -A OUTPUT -p tcp --syn -j REDIRECT --to-ports $TRANS_PORT
-
-    sudo iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-    for NET in $NON_TOR 127.0.0.0/8; do
-        sudo iptables -A OUTPUT -d $NET -j ACCEPT
-    done
-    sudo iptables -A OUTPUT -m owner --uid-owner $TOR_UID -j ACCEPT
-    sudo iptables -A OUTPUT -j REJECT
-    sudo service tor restart
-    """
-
     subprocess.call(['clear'], shell=True)
     print(colored(Start_Tor_Banner, 'magenta'))
-    subprocess.run(bash_start_tor, shell=True)
+    subprocess.run(['./bash_cmd/Start_Tor.sh'], shell=True)
     print(colored('[+] Tor Service is On ...', 'green'))
     print('\r\n')
-    
     anykay = input(colored("Enter anything to return to main menu : ", 'yellow'))
     MainMenu()
 
 
 def Stop_Tor():
-    bash_stop_tor = """\
-    sudo iptables -F
-    sudo iptables -t nat -F
-    sudo service tor stop
-    """
-    
     subprocess.call(['clear'], shell=True)
     print(colored(Stop_Tor_Banner, 'magenta'))
-    subprocess.run(bash_stop_tor, shell=True)
+    subprocess.run(['./bash_cmd/Stop_Tor.sh'], shell=True)
     print(colored('[+] Tor service is OFF ...', 'green'))
     print('\r\n')
-    
     anykay = input(colored("Enter anything to return to main menu : ", 'yellow'))
     subprocess.call(['clear'], shell=True)
     MainMenu()
@@ -118,17 +83,19 @@ def Torrc_Configuration():
 
         while True:
             try:
+                
                 selection = int(input("Enter Choice : "))
+                
                 if selection == 1:
-                    Chose_Nodes1()
+                    Entry_Nodes()
                     break
 
                 elif selection == 2:
-                    Chose_Nodes2()
+                    Exit_Nodes()
                     break
 
                 elif selection == 3:
-                    Chose_Nodes3()
+                    Exclude_Nodes()
                     break
 
                 elif selection == 4:
@@ -144,11 +111,13 @@ def Torrc_Configuration():
                 else:
                     print(colored('\nInvalide choise. Enter 1-6', 'red'))
                     Entry_Exit_Exclude()
+            
             except ValueError:
                 print(colored('\nInvalide choise. Enter 1-6', 'red'))
         exit
 
-    def Chose_Nodes1():
+    
+    def Entry_Nodes():
             terminal_menu = TerminalMenu(
                 [
         "ASCENSION ISLAND : {ac}", "AFGHANISTAN : {af}", "ALAND : {ax}", "ALBANIA : {al}", "ALGERIA : {dz}", "ANDORRA : {ad}", "ANGOLA : {ao}", "ANGUILLA : {ai}", "ANTARCTICA : {aq}", "ANTIGUA AND BARBUDA : {ag}", "ARGENTINA REPUBLIC : {ar}", "ARMENIA : {am}", "ARUBA : {aw}", "AUSTRALIA : {au}", "AUSTRIA : {at}", "AZERBAIJAN : {az}",
@@ -176,10 +145,7 @@ def Torrc_Configuration():
         "WALLIS AND FUTUNA ISLANDS : {wf}", "WESTERN SAHARA : {eh}",            
         "YEMEN : {ye}",
         "ZAMBIA : {zm}", "ZIMBABWE : {zw}"
-                ],
-                multi_select=True,
-                show_multi_select_hint=True,
-            )
+                ], multi_select=True, show_multi_select_hint=True,)
             
             menu_entry_indices = terminal_menu.show()
             print(colored('\nYour Entry Nodes Configuration : \n', 'magenta'))
@@ -188,11 +154,11 @@ def Torrc_Configuration():
             fichier.write(str(('\nEntryNodes ' + ",".join([item.split(" : ")[1] for item in terminal_menu.chosen_menu_entries]))))
             fichier.write(' StrictNodes 1')
             fichier.close()
-            
             anykay = input(colored("\nEnter anything to return to configuration menu : ", 'yellow'))
             Entry_Exit_Exclude()        
 
-    def Chose_Nodes2():
+    
+    def Exit_Nodes():
             terminal_menu = TerminalMenu(
                 [
         "ASCENSION ISLAND : {ac}", "AFGHANISTAN : {af}", "ALAND : {ax}", "ALBANIA : {al}", "ALGERIA : {dz}", "ANDORRA : {ad}", "ANGOLA : {ao}", "ANGUILLA : {ai}", "ANTARCTICA : {aq}", "ANTIGUA AND BARBUDA : {ag}", "ARGENTINA REPUBLIC : {ar}", "ARMENIA : {am}", "ARUBA : {aw}", "AUSTRALIA : {au}", "AUSTRIA : {at}", "AZERBAIJAN : {az}",
@@ -220,10 +186,7 @@ def Torrc_Configuration():
         "WALLIS AND FUTUNA ISLANDS : {wf}", "WESTERN SAHARA : {eh}",            
         "YEMEN : {ye}",
         "ZAMBIA : {zm}", "ZIMBABWE : {zw}"
-                ],
-                multi_select=True,
-                show_multi_select_hint=True,
-            )
+                ], multi_select=True, show_multi_select_hint=True,)
             
             menu_entry_indices = terminal_menu.show()
             print(colored('\nYour Exit Nodes Configuration : \n', 'magenta'))
@@ -231,12 +194,12 @@ def Torrc_Configuration():
             fichier = open("data/torrc.config", "a")
             fichier.write(str(('\nExitNodes ' + ",".join([item.split(" : ")[1] for item in terminal_menu.chosen_menu_entries]))))
             fichier.write(' StrictNodes 1')
-            fichier.close()
-            
+            fichier.close()  
             anykay = input(colored("\nEnter anything to return to configuration menu : ", 'yellow'))
             Entry_Exit_Exclude()
 
-    def Chose_Nodes3():
+    
+    def Exclude_Nodes():
             terminal_menu = TerminalMenu(
                 [
         "ASCENSION ISLAND : {ac}", "AFGHANISTAN : {af}", "ALAND : {ax}", "ALBANIA : {al}", "ALGERIA : {dz}", "ANDORRA : {ad}", "ANGOLA : {ao}", "ANGUILLA : {ai}", "ANTARCTICA : {aq}", "ANTIGUA AND BARBUDA : {ag}", "ARGENTINA REPUBLIC : {ar}", "ARMENIA : {am}", "ARUBA : {aw}", "AUSTRALIA : {au}", "AUSTRIA : {at}", "AZERBAIJAN : {az}",
@@ -264,49 +227,33 @@ def Torrc_Configuration():
         "WALLIS AND FUTUNA ISLANDS : {wf}", "WESTERN SAHARA : {eh}",            
         "YEMEN : {ye}",
         "ZAMBIA : {zm}", "ZIMBABWE : {zw}"
-                ],
-                multi_select=True,
-                show_multi_select_hint=True,
-            )
+                ], multi_select=True, show_multi_select_hint=True,)
             
             menu_entry_indices = terminal_menu.show()
             print(colored('\nYour Exclude Nodes Configuration : \n', 'magenta'))
             print(terminal_menu.chosen_menu_entries)
-            fichier = open("data/torrc.config", "a")
+            fichier = open("data/torrc.config.sh", "a")
             fichier.write(str(('\nExcludeNodes ' + ",".join([item.split(" : ")[1] for item in terminal_menu.chosen_menu_entries]))))
             fichier.close()
-            
             anykay = input(colored("\nEnter anything to return to configuration menu : ", 'yellow'))
             Entry_Exit_Exclude()
     
+    
     def Start_Torrc_Config():
-        
-        shifting = """\
-        
-        sudo mv -n /etc/tor/torrc /etc/tor/torrc.original
-        sudo mv data/torrc.config /etc/tor/torrc
-        cp data/torrc.start data/torrc.config
-        """
-
         subprocess.call(['clear'], shell=True)
         print(colored(Start_Tor_Banner, 'magenta'))
-        subprocess.call(shifting, shell=True)
+        subprocess.call(['./bash_cmd/Shifting.sh'] , shell=True)
         print(colored('[+] Configuration file is on : /etc/tor ', 'green'))
         print(colored('[+] Now go to the menu to launch Tor', 'green'))
-        
         anykay = input(colored("\nEnter anything to return to configuration menu : ", 'yellow'))
         Entry_Exit_Exclude()
 
+    
     def Remove_Torrc_Config():
-        Remove_Torrc = """\
-        sudo mv /etc/tor/torrc.original /etc/tor/torrc
-        """
-
         subprocess.call(['clear'])
         print(colored(Entry_Nodes_Banner, 'magenta'))
-        subprocess.call(Remove_Torrc, shell=True)
+        subprocess.call(['./bash_cmd/Remove_Torrc.sh'], shell=True)
         print(colored('[+] Configuration file is Remove ...', 'green'))
-
         anykay = input(colored("\nEnter anything to return to configuration menu : ", 'yellow'))
         Entry_Exit_Exclude()
 
@@ -320,7 +267,6 @@ def Check_IP():
     print(colored(Check_IP_Banner, 'magenta'))
     subprocess.call(['./bash_cmd/check_ip.sh'], shell=True)
     print('\n')
-    
     anykay = input(colored("Enter anything to return to main menu : ", 'yellow'))
     subprocess.call(['clear'], shell=True)
     MainMenu()
@@ -329,6 +275,14 @@ def Check_IP():
 def Quit():
     exit()
 
+
+Search_Onion_Banner = r'''
+   _____                      __        ____        _           
+  / ___/___  ____ ___________/ /_      / __ \____  (_)___  ____ 
+  \__ \/ _ \/ __ `/ ___/ ___/ __ \    / / / / __ \/ / __ \/ __ \
+ ___/ /  __/ /_/ / /  / /__/ / / /  _/ /_/ / / / / / /_/ / / / /
+/____/\___/\__,_/_/   \___/_/ /_/  (_)____/_/ /_/_/\____/_/ /_/ 
+'''                                                                
 
 Main_Menu_Banner = r'''
     ___    ____   ____         ______          
