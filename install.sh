6e84863b1a0d8cc8e702edd33c1ae2abcd7ae94d
x0Dsj <85474922+x0Dsj@users.noreply.github.com>
2021-06-30 17:27:19 +0200

    Add files via upload

--- install.sh
+++ install.sh
@@ -7,3 +7,7 @@ pip3 install subprocess.run
 pip3 install simple-term-menu 
 pip3 install termcolor 
 pip3 install requests
+sudo echo 'VirtualAddrNetwork 10.192.0.0/10' >> /etc/tor/torrc
+sudo echo 'AutomapHostsOnResolve 1' >> /etc/tor/torrc
+sudo echo 'TransPort 9040' >> /etc/tor/torrc
+sudo echo 'DNSPort 53' >> /etc/tor/torrc
