13ebb9ad2882f4e617559d801b6843cd761ab679
x0Dsj <85474922+x0Dsj@users.noreply.github.com>
2021-07-06 22:13:13 +0200

    Add files via upload

--- install.sh
+++ install.sh
@@ -2,8 +2,5 @@
 
 sudo apt update
 sudo apt install -y tor python3 python3-pip nyx
-pip3 install subprocess.run 
-pip3 install simple-term-menu 
-pip3 install termcolor 
-pip3 install requests
+pip3 install subprocess.run simple-term-menu termcolor requests
 echo -e "VirtualAddrNetwork 10.192.0.0/10\nAutomapHostsOnResolve 1\nTransPort 9040\nDNSPort 53" | sudo tee /etc/tor/torrc
