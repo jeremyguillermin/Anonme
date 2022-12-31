81fd6902381c042fd06c1cc1cf6ebae395d53cc9
jeremyguillermin <85474922+jeremyguillermin@users.noreply.github.com>
2022-12-31 18:00:54 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install -y tor python3 python3-pip nyx
-pip3 install subprocess.run simple-term-menu termcolor requests
+sudo apt install -y tor python3 python3-pip nyx && pip3 install subprocess.run simple-term-menu termcolor requests
 echo -e "VirtualAddrNetwork 10.192.0.0/10\nAutomapHostsOnResolve 1\nTransPort 9040\nDNSPort 53" | sudo tee /etc/tor/torrc
